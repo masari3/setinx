20936b3d61d89cfa356500b0b628af3726ba17fa
Fitri Ariyanto <seorang.fitri@gmail.com>
2025-09-05 11:50:38 +0700

    fix: type update vesion

--- setinx.sh
+++ setinx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# setupnginx.sh v1.1.10
+# setupnginx.sh v1.0.10
 
-VERSION="1.1.10"
+VERSION="1.0.10"
 PROJECTS_DIR="$HOME/Projects/www"
 NGINX_SITES_AVAILABLE="/usr/local/etc/nginx/sites-available"
 NGINX_SITES_ENABLED="/usr/local/etc/nginx/servers"
